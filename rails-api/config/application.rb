ecdd3c131646b1e98737d9a4b0393227e78af7f1
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-27 21:09:41 -0400

    allow configuring autoloader

--- rails-api/config/application.rb
+++ rails-api/config/application.rb
@@ -33,5 +33,9 @@ module RailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    if ENV['AUTOLOADER']
+      config.autoloader = ENV['AUTOLOADER'].to_sym
+    end
   end
 end
